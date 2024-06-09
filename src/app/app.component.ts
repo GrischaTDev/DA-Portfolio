475b6d7b18759524f2550a86d4cd48cdc9839a8a
Grischa <Grischa.Taenzer@gmail.com>
2024-06-10 01:21:18 +0200

    Update components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { HeroComponent } from "./hero/hero.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { ContactComponent } from "./contact/contact.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [
+      RouterOutlet, 
+      HeaderComponent, 
+      HeroComponent, 
+      AboutMeComponent, 
+      SkillsComponent, 
+      ContactComponent, 
+      PortfolioComponent, 
+      FooterComponent
+    ]
 })
 export class AppComponent {
   title = 'da-portfolio';
