dc8111a18dfef52616938c849a7d5fcd919c3eaa
Grischa <Grischa.Taenzer@gmail.com>
2024-06-10 01:15:52 +0200

    Create components

---
+++ src/app/pf-projects/pf-projects.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pf-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './pf-projects.component.html',
+  styleUrl: './pf-projects.component.scss'
+})
+export class PfProjectsComponent {
+
+}
