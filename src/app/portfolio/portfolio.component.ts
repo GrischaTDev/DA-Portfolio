acc95c0be2b86ef22de703300e1d90d6de767db9
Grischa <Grischa.Taenzer@gmail.com>
2024-06-20 20:37:41 +0200

    Update href and nav links

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -10,21 +10,30 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
 
-  portfolio = [ 
-    { name: 'Join', 
-      skills: 'Javascript | HTML | CSS', 
-      description: 'Task-Manager inspiriert vom Kanban-System. Erstellen und organisieren Sie Aufgaben mit Drag-and-Drop-Funktionen, weisen Sie Benutzer und Kategorien zu.', 
-      image: 'join'
+  portfolio = [
+    {
+      name: 'Join',
+      skills: 'Javascript | HTML | CSS',
+      description: 'Task-Manager inspiriert vom Kanban-System. Erstellen und organisieren Sie Aufgaben mit Drag-and-Drop-Funktionen, weisen Sie Benutzer und Kategorien zu.',
+      image: 'join',
+      test: 'https://grischataenzer.de/portfolio/join/index.html',
+      gitHub: 'https://github.com/GrischaTDev/Join-Solo'
     },
-    { name: 'Pokedex', 
-      skills: 'Javascript | HTML | CSS | Rest-API', 
-      description: 'Basierend auf der PokéAPI, eine einfache Bibliothek, die Details zu Pokémon-Arten, deren Entwicklungen, Fähigkeiten, Typen und Spielstatistiken enthält.', 
-      image: 'pokedex'
+    {
+      name: 'Pokedex',
+      skills: 'Javascript | HTML | CSS | Rest-API',
+      description: 'Basierend auf der PokéAPI, eine einfache Bibliothek, die Details zu Pokémon-Arten, deren Entwicklungen, Fähigkeiten, Typen und Spielstatistiken enthält.',
+      image: 'pokedex',
+      test: 'https://grischataenzer.de/portfolio/Pok%C3%A9dex/index.html',
+      gitHub: 'https://github.com/GrischaTDev/Pokedex-DA'
     },
-    { name: 'El Pollo Loco', 
-      skills: 'Javascript | HTML | CSS', 
-      description: 'Ein einfaches Jump-and-Run-Spiel, das auf einem objektorientierten Ansatz basiert. Hilf Pepe, Münzen und Giftflaschen zu finden, um gegen das Killerhuhn zu kämpfen.', 
-      image: 'pollo-loco'
+    {
+      name: 'El Pollo Loco',
+      skills: 'Javascript | HTML | CSS',
+      description: 'Ein einfaches Jump-and-Run-Spiel, das auf einem objektorientierten Ansatz basiert. Hilf Pepe, Münzen und Giftflaschen zu finden, um gegen das Killerhuhn zu kämpfen.',
+      image: 'pollo-loco',
+      test: 'https://grischataenzer.de/portfolio/El%20Pollo%20Loco/index.html',
+      gitHub: 'https://github.com/GrischaTDev/El-Pollo-Loco'
     },
   ];
 
